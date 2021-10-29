f5f71eb27b04f11bccf28b40261412c9a6902e15
shobhit saraogi <shobhitsaraogi@gmail.com>
2021-10-29 20:06:12 +0530

    list

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import requests
 
-proxya = {"https": "https://80.82.60.62:8080"}
-
-request = requests.get(url="https://ipinfo.io/json", timeout=10000)
+proxies = {
+  'http': 'http://10.10.1.10:3128',
+  'https': 'http://10.10.1.10:1080',
+}
+request = requests.get(url="https://ipinfo.io/json", timeout=100000 , proxies=proxies)
 print(request.text)
